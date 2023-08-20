e7057d6b5d387049e6f65970454e683526d02f05
Andrey <andreyturenkoit@gmail.com>
2023-08-20 23:26:46 +0300

    Add to fix

--- assets/js/hw-js-variables-prompt-conditions-cycles.js
+++ assets/js/hw-js-variables-prompt-conditions-cycles.js
@@ -1,30 +1,30 @@
-// Tasks: Variables
-// 1) Створіть дві змінні a та b. Виведіть у консоль результат їх множення.
+// // Tasks: Variables
+// // 1) Створіть дві змінні a та b. Виведіть у консоль результат їх множення.
 
 const a = 5;
 const b = 10;
 console.log(a * b);
 
-// 2) Створіть дві змінні c та d. Виведіть у консоль результат ділення першого на друге.
+// // 2) Створіть дві змінні c та d. Виведіть у консоль результат ділення першого на друге.
 
 const c = 50;
 const d = 16;
 console.log(c / d);
 
-// 3) Створіть дві змінні e та f. Виведіть у консоль результат додавання.
+// // 3) Створіть дві змінні e та f. Виведіть у консоль результат додавання.
 
 const e = 186;
 const f = 100000000;
 console.log(e + f);
 
-// 4) Визначте змінні зі значеннями 11, true, "java script", "100" і виведіть їх в консоль
+// // 4) Визначте змінні зі значеннями 11, true, "java script", "100" і виведіть їх в консоль
 
 console.log("11:", typeof 11);
 console.log("true:", typeof true);
 console.log("`java script:`", typeof `java script`);
 console.log('"100:"', typeof "100");
 
-// 5) Переробіть наведений код так, щоб у ньому використовувалися операції +=, -=, *=, /=, ++, --
+// // 5) Переробіть наведений код так, щоб у ньому використовувалися операції +=, -=, *=, /=, ++, --
 
 let num = 1;
 num += 11;
@@ -40,38 +40,38 @@ console.log(num);
 num--;
 console.log(num);
 
-//////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////
 
-// Tasks: Prompt
-// 1) Запитати число у користувача, піднести його до квадрату і вивести результат.
+// // Tasks: Prompt
+// // 1) Запитати число у користувача, піднести його до квадрату і вивести результат.
 
 const userSquareNumber = Number(prompt("Enter a number:"));
 alert(userSquareNumber * userSquareNumber);
 
-// 2) Вивести середнє арифметичне двох чисел. Числа запитувати у користувача.
+// // 2) Вивести середнє арифметичне двох чисел. Числа запитувати у користувача.
 
 const userArithmeticMean1 = +prompt("Enter the first number:");
 const userArithmeticMean2 = +prompt("Enter the second number:");
 alert(userArithmeticMean1 + userArithmeticMean2 / 2);
 
-// 3) Перевести кількість хвилин на секунди та вивести результат. Число хвилин запитувати у користувача.
+// // 3) Перевести кількість хвилин на секунди та вивести результат. Число хвилин запитувати у користувача.
 
 const userTime = +prompt("Enter the number of minutes:");
 alert(userTime * 60);
 
-// 4) Створіть змінну greeting зі значенням 'Hello,' і оголосіть змінну userName.
-// Запитайте ім'я користувача та внесіть це значення у змінну userName.
-// Виведіть повідомлення з привітанням, наприклад, 'Hello, Vasya!'. (згадайте про конкатенацію)
+// // 4) Створіть змінну greeting зі значенням 'Hello,' і оголосіть змінну userName.
+// // Запитайте ім'я користувача та внесіть це значення у змінну userName.
+// // Виведіть повідомлення з привітанням, наприклад, 'Hello, Vasya!'. (згадайте про конкатенацію)
 
 const greeting = "Hello, ";
 const userName = prompt("Your name?");
 console.log(greeting + userName);
 alert("Hi, Victoria!");
 
-////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////
 
-// Tasks: Conditions
-// 1) Запитати змінну у користувача. Якщо змінна дорівнює числу 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
+// // Tasks: Conditions
+// // 1) Запитати змінну у користувача. Якщо змінна дорівнює числу 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
 
 const userVariable = prompt("How much is 5x2 ?");
 if (userVariable === 10) {
@@ -80,10 +80,10 @@ if (userVariable === 10) {
   console.log("Incorrect");
 }
 
-// 2) Напишіть дві перевірки.
-// Якщо змінна test дорівнює true, виведіть 'Вірно', інакше виведіть 'Невірно'.
-// Якщо змінна test не дорівнює true, то виведіть 'Вірно', інакше виведіть 'Невірно'
-// Перевірте роботу скрипта при test, що дорівнює true, false.
+// // 2) Напишіть дві перевірки.
+// // Якщо змінна test дорівнює true, виведіть 'Вірно', інакше виведіть 'Невірно'.
+// // Якщо змінна test не дорівнює true, то виведіть 'Вірно', інакше виведіть 'Невірно'
+// // Перевірте роботу скрипта при test, що дорівнює true, false.
 
 const test = true;
 if (test) {
@@ -99,8 +99,8 @@ if (test) {
 //   console.log("False");
 // }
 
-// 3) Обчислити суму покупки з урахуванням знижки. Знижка 3% надається, якщо сума покупки більша за 500 грн.,
-// а знижка 5% - якщо сума покупки більша за 800 грн. Суму покупки вводить користувач.
+// // 3) Обчислити суму покупки з урахуванням знижки. Знижка 3% надається, якщо сума покупки більша за 500 грн.,
+// // а знижка 5% - якщо сума покупки більша за 800 грн. Суму покупки вводить користувач.
 
 const userPurchase = prompt("Your purchase amount:");
 let userDiscount;
@@ -117,42 +117,42 @@ if (userPurchase >= 500 && userPurchase < 800) {
   alert("Thanks for your purchase!");
 }
 
-/////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////
 
-// Tasks: Cycles
-// Кожне завдання вирішити циклами while, do..while.
-// 1) Виведення чисел від 25 до 0 (порядок зменшення).
+// // Tasks: Cycles
+// // Кожне завдання вирішити циклами while, do..while.
+// // 1) Виведення чисел від 25 до 0 (порядок зменшення).
 
 let currentNumber = 25;
 const TOTAL_NUMBER = 0;
 while (currentNumber >= TOTAL_NUMBER) {
   console.log("Number output:", currentNumber);
-  curentNumber--;
+  currentNumber--;
 }
 
-let currentNumbe = 25;
-const TOTAL_NUMBE = 0;
+let currentNumber2 = 25;
+const TOTAL_NUMBER2 = 0;
 do {
-  console.log("Number output:", currentNumbe);
-  currentNumbe--;
-} while (currentNumbe > TOTAL_NUMBE);
+  console.log("Number output:", currentNumber2);
+  currentNumber2--;
+} while (currentNumber2 >= TOTAL_NUMBER2);
 
-// 2) Виведення чисел від 10 до 50, які кратні 5.
+// // 2) Виведення чисел від 10 до 50, які кратні 5.
 
 let startNumber = 10;
 const LAST_NUMBER = 50;
 while (startNumber <= LAST_NUMBER) {
   if (startNumber % 5 === 0) {
-    console.log("Output numbers:", startNumber);
+    console.log("Number output:", startNumber);
   }
   startNumber++;
 }
 
 let firstNumber = 10;
 const LAST_ONE_NUMBER = 50;
-
 do {
-  if (firstNumber % 5 === 0);
-  console.log("Number output", firstNumber);
+  if (firstNumber % 5 === 0) {
+    console.log("Number output:", firstNumber);
+  }
   firstNumber++;
 } while (firstNumber <= LAST_ONE_NUMBER);
